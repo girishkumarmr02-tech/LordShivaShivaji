0ce385eb00285679d166c0f823a40e4598ddb0ee
Girish <girish.kumar.m.r02@gmail.com>
2026-07-12 11:43:11 +0530

    commit message

---
+++ Khambhaji/Palindrome.java
@@ -0,0 +1,39 @@
+package javaProgram;
+
+public class Palindrome {
+    public static void main(String[] args) {
+//        String str = "Mam";
+//        String rev = new StringBuilder("MaM").reverse().toString();
+//        if (str.equals(rev)) {
+//            System.out.println("its an palindrome");
+//        }
+//        else {
+//            System.out.println("Not a Palindrome");
+//        }
+
+//        String str = "bob";
+//        String rev = new StringBuilder("bob").reverse().toString();
+//        if(str.equals(rev)){
+//            System.out.println("its an palindrome");
+//        }
+//        else {
+//            System.out.println("not a palindrome");
+//        }
+
+        System.out.println("develop branch");
+        System.out.println("develop branch");
+        System.out.println("develop branch");
+        System.out.println("develop branch");
+
+        System.out.println("develop branch");
+
+
+        String str = "pop";
+        String rev = new StringBuilder("pop").reverse().toString();
+        if (str.equals(rev)){
+            System.out.println("its an palindrome");
+        }
+        else System.out.println("its not a palindrome");
+    }
+
+}
---
+++ Sambhaji/FibbanaciSeries.java
@@ -0,0 +1,27 @@
+package javaProgram;
+
+public class FibbanaciSeries {
+    public static void main(String[] args) {
+//        int a = 0, b = 1;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.print(a + " ");
+//            int sum = a + b;
+//            a = b;
+//            b = sum;
+//        }
+//        int a = 0, b = 1;
+//        for (int i =1;i<=10;i++){
+//            System.out.println(a + "");
+//            int sum = a + b;
+//            a = b;
+//            b = sum;
+//        }
+        int a = 0 , b= 1;
+        for (int i =1;i<=10;i++){
+            System.out.println(a + "");
+            int sum = a+b;
+            a = b;
+            b = sum;
+        }
+    }
+}
