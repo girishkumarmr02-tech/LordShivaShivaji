6210abcf7cdcfaed8ddfd59d5b005ff2bc604d6d
Girish <girish.kumar.m.r02@gmail.com>
2026-07-12 11:47:38 +0530

    commit message

---
+++ Chatrapathi /CountWordsinString copy.java
@@ -0,0 +1,27 @@
+package javaProgram;
+
+public class CountWordsinString {
+    public static void main(String[] args) {
+//        String str = "Sele java aut";
+//        String words[] = str.split("");
+//        System.out.println(words.length);
+
+//        String str = "Sele java aut";
+//        String words[] = str.split(" ");
+//
+//        System.out.println(words.length);
+
+        String str = "i Love Auto";
+        String words[] = str.split(" ");
+        System.out.println(words.length);
+
+        System.out.println("added by gittesting y user");
+
+        System.out.println("added by x user ");
+        System.out.println("added by x user ");
+        System.out.println("added by x user ");
+        System.out.println("added by x user ");
+        System.out.println("added by x user ");
+
+    }
+}
